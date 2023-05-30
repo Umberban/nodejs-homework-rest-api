1c80cfaf7201fe2ab9d0c65851c743cd26643a09
Umberban <romanovborysv@gmail.com>
2023-05-30 15:12:38 +0300

    deleted consolelog

--- middleware/bodyCheck.js
+++ middleware/bodyCheck.js
@@ -6,9 +6,7 @@ const validateBody=()=>{
             res.status(400).json({message: 'missing fields'})
             return;
         }
-        const validationResult = schema.validate(req.body);
         const {error} = schema.validate(req.body);
-        console.log(validationResult)
         if(error){
             res.status(400).json({ message: error.message })
             return;
