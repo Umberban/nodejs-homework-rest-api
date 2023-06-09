483a8f3fc3662f481701881793d16c6c15ff8edb
Umberban <romanovborysv@gmail.com>
2023-06-09 22:48:58 +0300

    Update bodyCheck.js

--- middleware/bodyCheck.js
+++ middleware/bodyCheck.js
@@ -1,6 +1,11 @@
-
 const validateBody=(schema)=>{
     return((req, res, next) => {
+        if(req.route.path==="/:contactId/favorite"){
+            if (Object.keys(req.body).length === 0) {
+                res.status(400).json({message: 'missing field favorite'})
+                return;
+            }
+        }
         if (Object.keys(req.body).length === 0) {
             res.status(400).json({message: 'missing fields'})
             return;
